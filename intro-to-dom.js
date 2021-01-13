0e8f5e08554342b5c41377ff8476130ec77dcb5f
Michael McInerney <michael.mcinerney@fullstackacademy.com>
2021-01-13 16:43:59 -0500

    Adding class examples

---
+++ intro-to-dom.js
@@ -0,0 +1,21 @@
+const buttonOne = document.getElementById('buttonOne');
+
+console.log(buttonOne);
+
+const allButtons = document.getElementsByTagName('button');
+
+console.log(allButtons);
+
+const allButtonArray = Array.from(allButtons);
+
+console.log(allButtonArray);
+
+allButtonArray.forEach((button) => {
+  console.log(button);
+});
+
+const parentNode = document.getElementById('parent');
+
+document.getElementsBy;
+
+console.log(parentNode.firstChild);
